e4b6a8e0cc1627dba340bf988a883c2b3a7d38a8
alejocardoso <aacar.ph@gmail.com>
2022-03-18 02:00:18 -0300

    a ver si funciona SA

--- script.js
+++ script.js
@@ -10,10 +10,6 @@ function aviso(){
         showConfirmButton: false,
         timer: 3000,
         timerProgressBar: true,
-        didOpen: (toast) => {
-          toast.addEventListener('mouseenter', Swal.stopTimer)
-          toast.addEventListener('mouseleave', Swal.resumeTimer)
-        }
       })
       
       Toast.fire({
